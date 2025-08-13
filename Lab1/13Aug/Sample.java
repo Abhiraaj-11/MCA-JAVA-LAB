c2d05b757a85555be707b7494464689fa85853fb
Abhiraj <abhiraj2k21@gmail.com>
2025-08-13 11:29:10 +0530

    Update Sample.java

--- Lab1/13Aug/Sample.java
+++ Lab1/13Aug/Sample.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
 
+class Sample{
+    void show() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter name school program and batch: ");
+        String name=sc.nextLine();
+        String school=sc.nextLine();
+        String program=sc.nextLine();
+        int batch = sc.nextInt();
+        sc.close();
+        System.out.println("NAME:" + name + "\nSchool: " + school + "\nProgram: " + program + "\nBatch: " + batch);
+    }
+
+    public static void main(String[] args) {
+        Sample obj = new Sample();
+        obj.show();
+    }
+}
