7f09d6cf3d3921e00fb806b4b90262961c1d6c00
Abhiraj <abhiraj2k21@gmail.com>
2025-08-20 10:17:32 +0530

    Create Sample.java

---
+++ 20Aug/Sample.java
@@ -0,0 +1,16 @@
+public class Sample {
+    public static void main(String[] args) {
+	System.out.println("Sum :"+(10+20));
+	int a=10;
+	System.out.println("Value of a:"+(++a));
+	int b=20;
+	System.out.println(a>b);
+	int num1= 5;  
+        int num2= 3; 
+        System.out.println("num1 & num2 = " + (num1 & num2));
+	boolean val1= true;
+        boolean val2= false;
+        System.out.println("val1 || val2 = " + (val1 || val2));
+
+    }
+}
